b7fc4d0fb759167467fd5ceb709788518ec0eccb
Johnny <lanqsh@hotmail.com>
2026-02-03 16:54:41 +0800

    :bug: 修复编译错误并简化消息负载

--- src/robot.cpp
+++ src/robot.cpp
@@ -4,7 +4,28 @@
 #include <sstream>
 
 Robot::Robot(const std::string& robot_id)
-    : robot_id_(robot_id), sequence_(0), battery_level_(100) {}
+    : robot_id_(robot_id), sequence_(0) {
+  // 初始化机器人数据
+  data_.battery_level = 100;
+  data_.battery_voltage = 0;
+  data_.battery_current = 0;
+  data_.battery_status = 0;
+  data_.battery_temperature = 0;
+  data_.main_motor_current = 0;
+  data_.slave_motor_current = 0;
+  data_.working_duration = 0;
+  data_.total_run_count = 0;
+  data_.current_lap_count = 0;
+  data_.solar_voltage = 0;
+  data_.solar_current = 0;
+  data_.board_temperature = 0;
+  data_.parking_position = 0;
+  data_.daytime_scan_protect = false;
+  data_.enabled = true;
+  data_.position = 0;
+  data_.direction = 0;
+  data_.domestic_foreign_flag = 0;
+}
 
 Robot::~Robot() {}
 
@@ -16,11 +37,9 @@ void Robot::SetTopics(const std::string& publish_topic,
 
 std::string Robot::GeneratePayload() {
   int seq = sequence_.fetch_add(1);
-  battery_level_ = 100 - (seq % 100);
 
   std::ostringstream oss;
-  oss << "{\"seq\": " << seq << ", \"battery\": " << battery_level_
-      << ", \"robot_id\": \"" << robot_id_ << "\"}";
+  oss << "{\"seq\": " << seq << ", \"robot_id\": \"" << robot_id_ << "\"}";
   return oss.str();
 }
 
